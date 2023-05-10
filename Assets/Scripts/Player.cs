6bd5a6c1f6fd73c43b54472715689c117dd1a46f
Muhammad Fahad Aldy <aldyaljufrie@gmail.com>
2023-05-10 12:28:06 +0700

    change platform, add mobile input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : Entity
 {
@@ -8,22 +9,41 @@ public class Player : Entity
 
     [SerializeField] private AudioClip laserAudio;
     [SerializeField] private float horizontalLimit = 2.5f;
+    private PlayerInput playerInput;
     private AudioSource audioSource;
-    private float cooldownTimer;
+    private float cooldownTimer, inputAxis;
     Rigidbody2D rb;
 
-    private void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.actions["Fire"].performed += Fire;
+    }
+
+    private void OnDisable()
+    {
+        playerInput.actions["Fire"].performed -= Fire;
     }
 
     private void Update()
     {
         cooldownTimer -= Time.deltaTime;
 
+#if UNITY_STANDALONE_WIN
+        if (Input.GetMouseButtonDown(0)) Fire();
+        inputAxis = Input.GetAxisRaw("Horizontal");
+#elif UNITY_ANDROID
+        inputAxis = playerInput.actions["Move"].ReadValue<float>();
+#endif
+
         // Player Movement
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0f);
+        rb.velocity = new Vector2(inputAxis * speed, 0f);
         if (transform.position.x > horizontalLimit)
         {
             rb.velocity = Vector2.zero;
@@ -35,25 +55,27 @@ public class Player : Entity
             rb.velocity = Vector2.zero;
             transform.position = new Vector3(-horizontalLimit, transform.position.y, transform.position.z);
         }
+    }
 
+    private void Fire(InputAction.CallbackContext ctx) => Fire();
+
+    private void Fire()
+    {
         // Player Attack
-        if (Input.GetMouseButtonDown(0))
+        if (cooldownTimer < 0)
         {
-            if (cooldownTimer < 0)
+            cooldownTimer = firingCooldown;
+            audioSource.PlayOneShot(laserAudio);
+
+            // Get gameobject from pool
+            GameObject laser = PlayerLaserPool.Instance.Get();
+            laser.transform.position = transform.position;
+
+            if (laser.TryGetComponent<Projectile>(out Projectile projectile))
             {
-                cooldownTimer = firingCooldown;
-                audioSource.PlayOneShot(laserAudio);
-
-                // Get gameobject from pool
-                GameObject laser = PlayerLaserPool.Instance.Get();
-                laser.transform.position = transform.position;
-
-                if (laser.TryGetComponent<Projectile>(out Projectile projectile))
-                {
-                    projectile.Init();
-                    projectile.CancelInvoke();
-                    projectile.Invoke("Release", projectile.lifetime);
-                }
+                projectile.Init();
+                projectile.CancelInvoke();
+                projectile.Invoke("Release", projectile.lifetime);
             }
         }
     }
